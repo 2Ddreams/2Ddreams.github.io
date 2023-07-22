ec07a61ea6ef3c0ce99883391e558b190c4d37d0
Harry <harry.zhu2006@outlook.com>
2023-07-22 14:04:43 +1000

    First Commit

---
+++ v2/Abby/AbbyMemory.js
@@ -0,0 +1,55 @@
+const backgroundMusic = document.getElementById('backgroundMusic');
+const musicIcon = document.querySelector('.music-icon');
+const timerElement = document.querySelector('.timer');
+const startTime = new Date('2021-03-06T00:00:00'); // Replace this with your desired start date
+let isMusicPlaying = false;
+
+function playMusic() {
+  backgroundMusic.play();
+  isMusicPlaying = true;
+  updateMusicIcon();
+}
+
+function pauseMusic() {
+  backgroundMusic.pause();
+  isMusicPlaying = false;
+  updateMusicIcon();
+}
+
+function toggleMusic() {
+  if (isMusicPlaying) {
+    pauseMusic();
+  } else {
+    playMusic();
+  }
+}
+
+function updateMusicIcon() {
+  const musicIconImage = musicIcon.querySelector('img');
+  if (isMusicPlaying) {
+    musicIconImage.setAttribute('src', '../music_icon_play.png'); // Replace with pause icon image
+  } else {
+    musicIconImage.setAttribute('src', '../music_icon_pause.png'); // Replace with play icon image
+  }
+}
+
+
+function updateTimer() {
+  const currentTime = new Date();
+  const timeDifference = currentTime - startTime;
+
+  const days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
+
+  timerElement.textContent = `Time Passed: ${days} days, ${hours} hours, ${minutes} minutes, ${seconds} seconds`;
+}
+
+// Call the updateTimer function every second
+setInterval(updateTimer, 1000);
+
+function contiButton(){
+    window.location.href = 'https://open.spotify.com/'; // Replace 'new_page.html' with your desired page URL
+
+}
---
+++ v2/MainPage.js
@@ -0,0 +1,46 @@
+const notification = document.querySelector('.notification');
+const passwordInput = document.getElementById('passwordInput');
+const container = document.querySelector('.container');
+const originalContent = container.innerHTML;
+let isMusicPlaying = false;
+
+// Add an event listener for "keypress" on the input field
+passwordInput.addEventListener('keypress', function (event) {
+  if (event.key === 'Enter') {
+    checkPassword();
+  }
+});
+
+function checkPassword() {
+  const enteredPassword = passwordInput.value;
+  const correctPassword = ['7604', 'admin'];
+
+  if (correctPassword.includes(enteredPassword)) {
+    // Fade out animation for current content
+    container.classList.add('fade-out');
+
+    // After the animation, load the new page
+    setTimeout(() => {
+      window.location.href = 'Abby/AbbyMemory.html'; // Replace 'new_page.html' with your desired page URL
+    }, 1000); // Wait for 1 second (adjust the time as needed based on your animation duration)
+  } else {
+    displayNotification('Incorrect password. Please try again.');
+  }
+}
+
+function displayNotification(message) {
+  notification.textContent = message;
+  notification.classList.add('show');
+
+  setTimeout(() => {
+    hideNotification();
+  }, 15000); // Set the duration (in milliseconds) for the notification to stay visible
+}
+
+function hideNotification() {
+  notification.classList.remove('show');
+}
+
+notification.addEventListener('animationend', () => {
+  hideNotification();
+});
